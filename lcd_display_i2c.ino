6e9067612606e600a162960a048292a2661b81e3
Naman .K <129218509+kohli-nk@users.noreply.github.com>
2024-06-25 22:51:37 +0530

    LCD Display

---
+++ lcd_display_i2c.ino
@@ -0,0 +1,19 @@
+#include <LiquidCrystal_I2C.h>
+
+#include <Wire.h>
+
+int counter = 0;
+LiquidCrystal_I2C lcd(0x27, 16, 2); // Set the LCD address to 0x27 for a 16 chars and 2 line display
+void setup() {
+  lcd.init();                       // Initialize the LCD
+  lcd.backlight();                  // Turn on the backlight
+  lcd.clear();                      // Clear the LCD screen
+}
+void loop() {
+  lcd.setCursor(0, 0);               // Set the cursor to the first column and first row
+  lcd.print("tf8ygg");     // Print some text
+  lcd.setCursor(0,1);
+  lcd.print(counter);
+  counter = counter + 1;
+  delay(500);                        // Wait for 1/2 seconds
+}
